aea1f98668a4c8b2cad16d092af34e01fcaf0d8e
rezwanrahimr <rezwanrahim69@gmail.com>
2022-09-14 20:52:06 +0600

    hearder added

--- src/pages/Sheard/Navbar.js
+++ src/pages/Sheard/Navbar.js
@@ -11,38 +11,38 @@ const Navbar = () => {
         
     </>
     return (
-        <div class="navbar bg-base-100">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" sstrokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         {nevbarItem}
                     </ul>
                 </div>
                 <div className='flex items-center'>
-                    <a class="btn btn-ghost normal-case text-xl"><img width={'50px'} src={Logo} alt="" /></a>
+                    <a className="btn btn-ghost normal-case text-xl"><img width={'50px'} src={Logo} alt="" /></a>
                     <span className='font-bold text-lg'>DOCTORS POINT</span>
                 </div>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal p-0">
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
                     {nevbarItem}
                 </ul>
             </div>
-            <div class="navbar-end">
-                <div class="dropdown dropdown-end">
-                    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                        <div class="w-10 rounded-full">
+            <div className="navbar-end">
+                <div className="dropdown dropdown-end">
+                    <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
                             <img src="https://placeimg.com/80/80/people" />
                         </div>
                     </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         <li>
-                            <a class="justify-between">
+                            <a className="justify-between">
                                 Profile
-                                <span class="badge">New</span>
+                                <span className="badge">New</span>
                             </a>
                         </li>
                         <li><a>Settings</a></li>
